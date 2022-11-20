4630cc05f06d672b60723ca25ef18a43b6c7dd72
Helen Liu <HelenLiu12345@gmail.com>
2022-11-20 17:54:06 -0500

    timeblock response/util

---
+++ server/timeblock/util.ts
@@ -0,0 +1,53 @@
+import type {HydratedDocument} from 'mongoose';
+import moment from 'moment';
+import type {TimeBlock, PopulatedTimeBlock} from '../timeblock/model';
+
+// Update this if you add a property to the TimeBlock type!
+type TimeBlockResponse = {
+  _id: string;
+  owner: string;
+  requester: string;
+  start: string;
+  claimed: boolean;
+  accepted: boolean;
+  occurred: boolean;
+};
+
+/**
+ * Encode a date as an unambiguous string
+ *
+ * @param {Date} date - A date object
+ * @returns {string} - formatted date as string
+ */
+const formatDate = (date: Date): string => moment(date).format('MMMM Do YYYY, h:mm:ss a');
+
+/**
+ * Transform a raw TimeBlock object from the database into an object
+ * with all the information needed by the frontend
+ *
+ * @param {HydratedDocument<TimeBlock>} timeBlock - A time block
+ * @returns {TimeBlockResponse} - The timeBlock object formatted for the frontend
+ */
+const constructTimeBlockResponse = (timeBlock: HydratedDocument<TimeBlock>): TimeBlockResponse => {
+  const timeBlockCopy: PopulatedTimeBlock = {
+    ...timeBlock.toObject({
+      versionKey: false // Cosmetics; prevents returning of __v property
+    })
+  };
+  const owner = timeBlockCopy.owner.username;
+  delete timeBlockCopy.owner;
+  const requester = (timeBlockCopy.requester) ? timeBlockCopy.requester.username : null;
+  const occurred = (timeBlockCopy.occurred) ? timeBlockCopy.occurred : null;
+  return {
+    ...timeBlockCopy,
+    _id: timeBlockCopy._id.toString(),
+    owner: owner,
+    requester: requester,
+    start: formatDate(timeBlock.start),
+    occurred: occurred,
+  };
+};
+
+export {
+  constructTimeBlockResponse
+};
