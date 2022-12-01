7e3856f9750a50fd38af08d36001d4a6b1b6f0f9
tejalreddy <66268918+tejalreddy@users.noreply.github.com>
2022-12-01 10:22:40 -0500

    added tags to user card (#18)
    Co-authored-by: Tejal Reddy <reddyt@mit.edu>

--- client/components/Search/UserCardComponent.vue
+++ client/components/Search/UserCardComponent.vue
@@ -8,6 +8,10 @@
     <div class="otherInfo">
       <i>Class of {{ user.graduationYear }}</i>
       <p><b>Industry: {{ user.industry !== undefined ? user.industry : "None" }}</b></p>
+      <p v-if="tagsDisplayed.length > 0">
+        I am willing to: {{ tagsDisplayed.join(', ') }}</p>
+        <p v-if="tagsDisplayed.length === 0">
+        I am willing to: None selected</p>
     </div>
   </section>
 </template>
@@ -20,6 +24,43 @@ export default {
             type: Object,
             required: true
         }
+    },
+    data() {
+      return {
+        tags: {},
+        frontEndTags: {
+            'refer': 'Write referrals',
+            'resumeReview': 'Review resumes',
+            'mentor': 'Provide mentoring',
+            'coffeeChat': 'Coffee Chat',
+            'helpInterview': 'Help with Interview Preparation',
+            'email': 'Email'
+        },
+        tagsDisplayed: []
+      }
+    },
+    mounted() {
+      this.getTags();
+    },
+    watch: {
+      tags: function(val) {
+            this.tagsDisplayed = [];
+            for (let tag in this.tags) {
+                if (this.tags[tag] === true) {
+                  this.tagsDisplayed.push(this.frontEndTags[tag])
+                }
+            }
+        }
+    },
+    methods: {
+      async getTags() {
+            const r = await fetch(`/api/tags/${this.user._id}`);
+            const res = await r.json();
+            if (!r.ok) {
+                throw new Error(res.error);
+            }
+            this.tags = res.tags;
+        },
     }
 }
 </script>
