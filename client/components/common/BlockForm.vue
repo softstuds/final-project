652318fab3f91997cd989d85465e6517ae72fe8b
tejalreddy <66268918+tejalreddy@users.noreply.github.com>
2022-12-02 08:24:12 -0500

    Tags frontend bugs (#20)
    * tags only created when new account is made
    
    * add uservalidator middlware
    
    * removed console logs
    
    Co-authored-by: Tejal Reddy <reddyt@mit.edu>

--- client/components/common/BlockForm.vue
+++ client/components/common/BlockForm.vue
@@ -69,6 +69,7 @@ export default {
       method: 'GET', // Form request method
       hasBody: false, // Whether or not form request has a body
       setUser: false, // Whether or not stored username should be updated after form submission
+      newUser: false, // if a new user is being created
       refreshUser: false, // Whether or not to refresh user info
       deleteUser: false, // If the user info has been deleted
       alerts: {}, // Displays success/error messages encountered during form submission
@@ -127,15 +128,18 @@ export default {
           this.$store.commit('setUserId', res.user ? res.user._id.toString() : null);
           this.$store.commit('getUsers');
 
+          if (this.newUser && res.user) {
           // create tags when a new user is created
           const options = {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            credentials: 'same-origin' // Sends express-session credentials with request
-          };
-          const tags = await fetch('/api/tags', options);
-          const tagsRes = await tags.json();
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                credentials: 'same-origin' // Sends express-session credentials with request
+              };
+              const tags = await fetch('/api/tags', options);
+              const tagsRes = await tags.json();
         }
+        }
+
 
         if (this.refreshUser) {
           this.$store.commit('refreshUser');
