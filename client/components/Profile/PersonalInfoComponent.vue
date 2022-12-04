7e661b700c8bca701f6e2f725627c2c3aa26ab5b
Tejal Reddy <reddyt@mit.edu>
2022-12-04 15:32:01 -0500

    fixed tags route call

--- client/components/Profile/PersonalInfoComponent.vue
+++ client/components/Profile/PersonalInfoComponent.vue
@@ -124,7 +124,7 @@ export default {
       this.user = res.user;
     },
     async getTags() {
-      const r = await fetch(`/api/tags/${this.userId}`);
+      const r = await fetch(`/api/tags/users/${this.userId}`);
       const res = await r.json();
       if (!r.ok) {
           throw new Error(res.error);
