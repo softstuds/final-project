8a9a4a29c208575b22637b5b1b86d808cd1321dd
jasmine <jasmine2000@gmail.com>
2022-12-04 15:33:09 -0500

    meeting link form (#28)
    * modify routes + add meeting link form
    
    * docstring changes

--- client/components/Account/AccountPage.vue
+++ client/components/Account/AccountPage.vue
@@ -7,6 +7,7 @@
       <header>
         <h2>Account settings for {{ $store.state.user.firstName + ' ' + $store.state.user.lastName }}</h2>
       </header>
+      <ChangeMeetingLinkForm :originalValue="$store.state.user.meetingLink ?? ''"/>
       <ChangeEmailForm :originalValue="$store.state.user.email"/>
       <ChangePasswordForm />
     </section>
@@ -21,6 +22,7 @@
 </template>
 
 <script>
+import ChangeMeetingLinkForm from '@/components/Account/ChangeMeetingLinkForm.vue';
 import ChangeEmailForm from '@/components/Account/ChangeEmailForm.vue';
 import ChangePasswordForm from '@/components/Account/ChangePasswordForm.vue';
 import DeleteAccountForm from '@/components/Account/DeleteAccountForm.vue';
@@ -29,6 +31,7 @@ import LogoutForm from '@/components/Account/LogoutForm.vue';
 export default {
   name: 'AccountPage',
   components: {
+    ChangeMeetingLinkForm,
     ChangeEmailForm,
     ChangePasswordForm,
     DeleteAccountForm,
