1f49cf07c9daff3018300702b0bd272b6f8ed593
Nina <ninayw@mit.edu>
2022-12-04 20:18:14 -0500

    meeting link bug

--- client/components/Meeting/MeetingComponent.vue
+++ client/components/Meeting/MeetingComponent.vue
@@ -73,23 +73,24 @@ export default {
         if (this.user.email == this.meeting.owner) {
           this.link = this.$store.state.user.meetingLink == '' ? "no link yet" : this.$store.state.user.meetingLink;
         } else {
-            try {
-              const r = await fetch(`/api/users/${this.meeting.owner}`, {
-                method: 'GET', 
-                headers: {'Content-Type': 'application/json'},
-              });
+          this.link = `${this.meeting.owner}'s meeting link`
+            // try {
+            //   const r = await fetch('/api/users/find', {
+            //     method: 'GET', 
+            //     headers: {'Content-Type': 'application/json'}
+            //   });
+            //   console.log('hi');
+            //   const res = await r.json();
+            //   if (!r.ok) {
+            //     throw new Error(res.error);
+            //   }
 
-              const res = await r.json();
-              if (!r.ok) {
-                throw new Error(res.error);
-              }
+            //   this.link = res.user.meetingLink == '' ? "no link yet" : this.$store.state.user.meetingLink;
 
-              this.link = res.user.meetingLink == '' ? "no link yet" : this.$store.state.user.meetingLink;
-
-            } catch (e) {
-              this.$set(this.alerts, e, 'error');
-              setTimeout(() => this.$delete(this.alerts, e), 3000);
-          } 
+            // } catch (e) {
+            //   this.$set(this.alerts, e, 'error');
+            //   setTimeout(() => this.$delete(this.alerts, e), 3000);
+            // } 
         }
     }
       
