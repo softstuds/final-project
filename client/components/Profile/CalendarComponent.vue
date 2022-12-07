99e37fe20c016c8978a6a5913ea32843829d15c0
jasmine2000 <jasmine2000@gmail.com>
2022-12-07 09:49:02 -0500

    indentation

--- client/components/Profile/CalendarComponent.vue
+++ client/components/Profile/CalendarComponent.vue
@@ -333,9 +333,7 @@ export default {
                 method: 'PUT',
                 headers: {'Content-Type': 'application/json'},
                 credentials: 'same-origin',
-                body: JSON.stringify({
-                  start: date.toString(),
-                })
+                body: JSON.stringify({start: date.toString()})
             };
 
             try {
