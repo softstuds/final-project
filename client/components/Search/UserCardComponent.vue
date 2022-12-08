412a5f6076f619b189584f4256e06b757050aeae
Tejal Reddy <reddyt@mit.edu>
2022-12-08 12:09:18 -0500

    formatted information on user card

--- client/components/Search/UserCardComponent.vue
+++ client/components/Search/UserCardComponent.vue
@@ -7,11 +7,19 @@
     </router-link>
     <div class="otherInfo">
       <i>Class of {{ user.graduationYear }}</i>
-      <p><b>Industry: {{ user.industry !== undefined ? user.industry.industryType : "None" }}</b></p>
-      <p v-if="tagsDisplayed.length > 0">
-        I am willing to: {{ tagsDisplayed.join(', ') }}</p>
-        <p v-if="tagsDisplayed.length === 0">
-        I am willing to: None selected</p>
+      <p class="industry-info"><b>Industry: {{ user.industry !== undefined ? user.industry.industryType : "None" }}</b></p>
+      <p 
+        class="tags-info"
+        v-if="tagsDisplayed.length > 0">
+        I am willing to:
+        <li class="tags-list"
+        v-for="tag in tagsDisplayed">{{ tag }}</li>
+      </p>
+      <p 
+        class="tags-info"
+        v-if="tagsDisplayed.length === 0">
+        I am willing to: Unspecified
+      </p>
     </div>
   </section>
 </template>
@@ -70,14 +78,32 @@ export default {
     padding: 10px;
     padding-top: 20px;
     margin: 5px;
+    position: relative;
 }
 
 .name {
     font-size: larger
 }
-
 .otherInfo {
     font-size: medium;
     margin-top: 10px;
+}
+
+.tags-info {
+  font-size: 14px;
+  width: 40%;
+  line-height: 2px;
+}
+
+.tags-list {
+  font-size: 14px;
+  margin-bottom: 0px;
+}
+
+.industry-info {
+  position: absolute;
+  top: 0px;
+  right: 10px;
+  font-size: 16px;
 }
 </style>
