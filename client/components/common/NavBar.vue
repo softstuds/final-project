413faaccc4b1f7a355907df2fd9d6b900f5833fe
Tejal Reddy <reddyt@mit.edu>
2022-12-13 16:15:23 -0500

    fixed other navbar things

--- client/components/common/NavBar.vue
+++ client/components/common/NavBar.vue
@@ -11,7 +11,9 @@
       </h1>
     </div>
     <div class="right">
-      <router-link to="/">
+      <router-link 
+        v-if="$store.state.userId"
+        to="/home">
         Home
       </router-link>
       <router-link
